2f706fa2bc6150c4f0485385258ad835ab1b22a3
r02ajat08 <168282130+r02ajat08@users.noreply.github.com>
2025-01-08 10:31:29 +0000

    4

--- js_1/01.js
+++ js_1/01.js
@@ -1,11 +1,21 @@
-console.log("hey");
-console.log(result);
+// console.log("hey");
+// console.log(result);
 
-// for (let index = 0; index < 2; index++) {
-//     var result = 12;
-//     console.log(result);
-// }
-var result = 2;
-console.log(result);
-let hull = null;
-console.log(typeof hull)
+// // for (let index = 0; index < 2; index++) {
+// //     var result = 12;
+// //     console.log(result);
+// // }
+// var result = 2;
+// console.log(result);
+// let hull = null;
+// console.log(typeof hull)
+
+// let num = 5
+// console.log(num); //5
+// num++;
+// console.log(num); //6
+
+let num2 = 5
+console.log(num2); //5
+(++num2);
+console.log(num2); //6
