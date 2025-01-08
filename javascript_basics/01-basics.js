7b1796deac786b4cbda3912bfe6ecef76c2e9fb3
r02ajat08 <168282130+r02ajat08@users.noreply.github.com>
2025-01-08 02:40:37 +0000

    day-2

--- javascript_basics/01-basics.js
+++ javascript_basics/01-basics.js
@@ -1,1 +1,9 @@
-console.log("hey")
+console.log("hey");
+console.log(result);
+
+// for (let index = 0; index < 2; index++) {
+//     var result = 12;
+//     console.log(result);
+// }
+var result = 2;
+console.log(result);
