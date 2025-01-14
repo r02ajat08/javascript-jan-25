903339ad1785b0769b49010c3d7a59c4ec930ef5
r02ajat08 <168282130+r02ajat08@users.noreply.github.com>
2025-01-14 03:36:47 +0000

    day-8 morning

--- 03_basics/01_basics.js
+++ 03_basics/01_basics.js
@@ -27,5 +27,29 @@ function loginUserName(username = "agrim "){
     // }
     return `${username}just logged in`
 }
-console.log(loginUserName());
-console.log(loginUserName("rajat "));
+// console.log(loginUserName());
+// console.log(loginUserName("rajat "));
+
+function cartValue(var1,var2,...num1){  //important concept
+    return num1
+}
+// console.log(cartValue(200,222,333,444,555));
+const user={
+    username: "Rajat",
+    id:2  //if did mistake in id by adding "s" like ids, output will be undefined  
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and id is ${anyobject.id}`)
+}
+// handleObject(user)
+// handleObject({
+//     username: "agrim",
+//     id:1
+// }) // it works with the new declared variables
+const newArray = [200,300,400,500]
+
+function returnTheSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnTheSecondValue(newArray));
+// console.log(returnTheSecondValue([20,30,40,50])); //look the syntax carefully
---
+++ 03_basics/02_scope.js
@@ -0,0 +1,52 @@
+let a = 20;
+const b = 30;
+var c = 40 ;
+if (true){
+    let a =200;
+    const b = 300;
+    var c =400 
+    // console.log("inner: ",a);
+    // console.log(`inner: ${b}`);
+    // console.log(`inner: ${c}`);
+
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function one(){
+    const name = "agrim"
+    function two(){
+        const website = "google"
+        // console.log(name);
+        
+    }
+    // console.log(website);
+    two();
+}
+one();
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+//  this is scope   "{}"   function and if else, for loop, while loop
+if (true){
+    const username = "java"
+    if (true){
+        const website = " github"
+        // console.log(username + website);
+        
+    }
+    // console.log(username + website);
+}
+// console.log(username);
+
+// +++++++++++++++++++interesting+++++++++++++++++++++++
+function addOne(num){
+    return num + 1
+}
+// console.log(addOne(5));
+
+const addtwo = function(num){
+
+    return num + 2
+}
+console.log(addtwo(5));
